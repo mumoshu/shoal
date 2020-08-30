aa0c90e11aaa5a777ed2300b6500eca37a2260e4
Yusuke Kuoka <ykuoka@gmail.com>
2020-08-30 13:22:03 +0900

    Add missing cmd/ package

---
+++ cmd/shoal/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/mumoshu/shoal"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+func main() {
+	var configFile string
+
+	flag.StringVar(&configFile, "f", "shoal.yaml", "Path to the config file")
+
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		fmt.Fprintf(os.Stdout, "%s\n", shoal.Version)
+		os.Exit(0)
+	}
+
+	f, err := os.Open(configFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening %s: %v\n", configFile, err)
+		os.Exit(1)
+	}
+
+	var config shoal.Config
+
+	d := yaml.NewDecoder(f)
+
+	if err := d.Decode(&config); err != nil {
+		fmt.Fprintf(os.Stderr, "Error decoding yaml file %q: %v\n", configFile, err)
+		os.Exit(1)
+	}
+
+	app, err := shoal.New()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := app.Sync(config); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
